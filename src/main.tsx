7b51bb24cd4cfc6cc552f7f4bf153dd531a00422
vegar24sfj <your-email@example.com>
2025-06-18 04:29:33 +0200

    lagt til clerk i main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
+// Hent Clerk-nøkkelen fra .env
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key");
+}
+
+// Pakk inn App med ClerkProvider
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
